d8dbde4b33dc8388748bf3464229935f8258f2a7
Alberto <albergap@gmail.com>
2020-03-05 13:13:34 +0100

    Initial commit

---
+++ src/main/java/com/uc3m/fs/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.uc3m.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
