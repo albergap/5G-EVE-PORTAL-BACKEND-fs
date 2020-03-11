fc265472cf9206bb8f7922a70ef7ee50bf9dfad4
Alberto <albergap@gmail.com>
2020-03-11 14:35:53 +0100

    Parameters validation with annotations

--- src/main/java/com/uc3m/fs/FS_Controller.java
+++ src/main/java/com/uc3m/fs/FS_Controller.java
@@ -3,11 +3,15 @@ package com.uc3m.fs;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Base64;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +29,7 @@ import com.uc3m.fs.storage.exceptions.StorageFileNotFoundException;
 
 @RestController
 @RequestMapping(Config.PATH)
+@Validated
 public class FS_Controller {
 	// Upload a MultipartFile (without chunks):
 	// https://spring.io/guides/gs/uploading-files
@@ -47,37 +52,46 @@ public class FS_Controller {
 							));
 			return new ResponseEntity<>(b64, HttpStatus.OK);
 		} catch (StorageFileNotFoundException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		} catch (Exception e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PostMapping(value = "upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	@ResponseBody
-	public ResponseEntity<String> upload(
-			@RequestPart("file") MultipartFile file,
-			@RequestParam(name = "dzuuid", required = true) String uuid,
-			@RequestParam(name = "List<site>", required = true) String[] sites) {
+	public ResponseEntity<Void> upload(
+			@RequestPart(name = "file", required = true) MultipartFile file,
+			@RequestParam(name = "dzuuid", required = true) @NotEmpty String uuid,
+			@RequestParam(name = "List<site>", required = false) String[] sites) {
 		try {
 			if (uuid == null || uuid.isEmpty())
 				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
-			for (int i = 0; i < sites.length; i++)
-				if (sites[i].isEmpty())
-					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			if (sites != null) {
+				for (int i = 0; i < sites.length; i++)
+					if (sites[i].isEmpty())
+						return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
 
 			storageService.store(file, uuid);
 			return new ResponseEntity<>(HttpStatus.ACCEPTED);
 		} catch (FileAlreadyExistsException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
 		} catch (Exception e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException e) {
+		return ResponseEntity.badRequest().build();
+	}
+
 	@ExceptionHandler(MaxUploadSizeExceededException.class)
-	public ResponseEntity<?> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exc) {
+	public ResponseEntity<?> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
 		return ResponseEntity.badRequest().build();
 	}
 
